749dc6cd32adb1e00cfd146b4057a3588c1a0122
Schwyi <lucasmirandoliveira@gmail.com>
2021-01-28 21:08:29 -0300

    Heroku PORT fixed

--- server.js
+++ server.js
@@ -32,4 +32,10 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT || 3000)
+
+const PORT = process.env.PORT || 3000
+
+
+app.listen( PORT, () => {
+  console.log(`app is running on port ${PORT}`);
+})
